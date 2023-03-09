6d1ed8e76fbd922203ce1f13fbb33c2c399835a6
Caleb <calebgif@umich.edu>
2023-03-09 16:49:52 -0500

    Started on a pit prefab

--- EECS494-P3/Assets/Scripts/IsPit.cs
+++ EECS494-P3/Assets/Scripts/IsPit.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class IsPit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log("Fell into a pit");
+        Vector3 fellPos = other.transform.position;
+        fellPos.z += 2;
     }
 }
