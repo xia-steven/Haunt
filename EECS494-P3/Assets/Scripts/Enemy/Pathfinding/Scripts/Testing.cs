90c59a40a15329acf620af59660888d551aa2a85
Steven Xia <stevenx@umich.edu>
2023-03-11 18:06:43 -0500

    added pathfinding scripts

---
+++ EECS494-P3/Assets/Scripts/Enemy/Pathfinding/Scripts/Testing.cs
@@ -0,0 +1,45 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour {
+    [SerializeField] private PathfindingDebugStepVisual pathfindingDebugStepVisual;
+    [SerializeField] private PathfindingVisual pathfindingVisual;
+    [SerializeField] private CharacterPathfindingMovementHandler characterPathfinding;
+    private Pathfinding pathfinding;
+
+    private void Start() {
+        pathfinding = new Pathfinding(20, 10);
+        pathfindingDebugStepVisual.Setup(pathfinding.GetGrid());
+        pathfindingVisual.SetGrid(pathfinding.GetGrid());
+        InvokeRepeating(nameof(findNewPath), 0.1f, 0.6f);
+    }
+
+    private void findNewPath() {
+        if (Input.GetMouseButton(0)) {
+            var mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
+            pathfinding.GetGrid().GetXY(mouseWorldPosition, out var x, out var y);
+            pathfinding.FindPath(0, 0, x, y);
+            characterPathfinding.SetTargetPosition(mouseWorldPosition);
+        }
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(1)) {
+            var mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
+            pathfinding.GetGrid().GetXY(mouseWorldPosition, out var x, out var y);
+            pathfinding.GetNode(x, y).SetIsWalkable(!pathfinding.GetNode(x, y).isWalkable);
+        }
+    }
+}
--- EECS494-P3/Assets/_/Base/BaseScripts/GameHandler_Setup.cs
+++ EECS494-P3/Assets/_/Base/BaseScripts/GameHandler_Setup.cs
@@ -1,42 +1,67 @@
-﻿using System.Collections;
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey;
 using CodeMonkey.Utils;
 using CodeMonkey.MonoBehaviours;
+using GridPathfindingSystem;
 
 public class GameHandler_Setup : MonoBehaviour {
 
+    public static GridPathfinding gridPathfinding;
     [SerializeField] private CameraFollow cameraFollow;
-    private Vector3 cameraPosition;
-    private float orthoSize = 60f;
+    [SerializeField] private CharacterAimHandler characterAimHandler;
 
     private void Start() {
-        cameraPosition = new Vector3(84, 38);
-        cameraFollow.Setup(() => cameraPosition, () => orthoSize, true, true);
-    }
+        //Sound_Manager.Init();
+        cameraFollow.Setup(GetCameraPosition, () => 60f, true, true);
 
-    private void Update() {
-        float cameraSpeed = 100f;
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
-            cameraPosition += new Vector3(-1, 0) * cameraSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
-            cameraPosition += new Vector3(+1, 0) * cameraSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-            cameraPosition += new Vector3(0, +1) * cameraSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-            cameraPosition += new Vector3(0, -1) * cameraSpeed * Time.deltaTime;
-        }
+        //FunctionPeriodic.Create(SpawnEnemy, 1.5f);
+        //for (int i = 0; i < 1000; i++) SpawnEnemy();
         
-        if (Input.GetKeyDown(KeyCode.KeypadPlus)) {
-            orthoSize -= 10f;
-        }
-        if (Input.GetKeyDown(KeyCode.KeypadMinus)) {
-            orthoSize += 10f;
+        gridPathfinding = new GridPathfinding(new Vector3(-400, -400), new Vector3(400, 400), 5f);
+        gridPathfinding.RaycastWalkable();
+
+        EnemyHandler.Create(new Vector3(20, 0));
+
+        characterAimHandler.OnShoot += CharacterAimHandler_OnShoot;
+    }
+
+    private void CharacterAimHandler_OnShoot(object sender, CharacterAimHandler.OnShootEventArgs e) {
+        Shoot_Flash.AddFlash(e.gunEndPointPosition);
+        WeaponTracer.Create(e.gunEndPointPosition, e.shootPosition);
+        UtilsClass.ShakeCamera(.6f, .05f);
+
+        // Any enemy hit?
+        RaycastHit2D raycastHit = Physics2D.Raycast(e.gunEndPointPosition, (e.shootPosition - e.gunEndPointPosition).normalized, Vector3.Distance(e.gunEndPointPosition, e.shootPosition));
+        if (raycastHit.collider != null) {
+            EnemyHandler enemyHandler = raycastHit.collider.gameObject.GetComponent<EnemyHandler>();
+            if (enemyHandler != null) {
+                enemyHandler.Damage(characterAimHandler);
+            }
         }
     }
 
+    private Vector3 GetCameraPosition() {
+        Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
+        Vector3 playerToMouseDirection = mousePosition - characterAimHandler.GetPosition();
+        return characterAimHandler.GetPosition() + playerToMouseDirection * .3f;
+    }
+
+    private void SpawnEnemy() {
+        Vector3 spawnPosition = characterAimHandler.GetPosition() + UtilsClass.GetRandomDir() * 40f;// 100f;
+        EnemyHandler.Create(spawnPosition);
+    }
 }
