7419c936752399b1c4e8fb63591c94341f8ea472
Caleb <calebgif@umich.edu>
2023-03-17 21:17:48 -0400

    Added sounds to pedestals

--- EECS494-P3/Assets/Scripts/Pedestal/HasPedestalHealth.cs
+++ EECS494-P3/Assets/Scripts/Pedestal/HasPedestalHealth.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [RequireComponent(typeof(IsPedestal))]
 public class HasPedestalHealth : HasHealth {
     [SerializeField] int PedestalMaxHealth = 5;
+    [SerializeField] AudioClip damageSound;
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] AudioClip healSound;
+    [SerializeField] AudioClip restoreSound;
 
     IsPedestal pedestal;
 
@@ -18,40 +22,33 @@ public class HasPedestalHealth : HasHealth {
         // NOTE: health delta is treated backwards of standard health components
         // due to the player destroying pedestals and enemies 
         healthDelta = -healthDelta;
-        if (health == 0 && healthDelta < 0) return false;
+        if (health == 0 && healthDelta < 0 || health == PedestalMaxHealth && healthDelta > 0 || healthDelta == 0) return false;
 
-        if (health + healthDelta >= PedestalMaxHealth) {
-            if (pedestal.IsDestroyedByPlayer())
-            {
-                // Increase pedestal max health for the player to destroy
-                PedestalMaxHealth++;
-                health = PedestalMaxHealth;
-                // Let other systems know the enemies repaired a pedestal
-                pedestal.PedestalRepaired();
-            }
-            else
-            {
-                health = PedestalMaxHealth;
+        health = Mathf.Clamp(health + healthDelta, 0, PedestalMaxHealth);
 
-            }
-            pedestal.updateVisuals(health, PedestalMaxHealth);
-            return false;
+        if (health <= 0 && !pedestal.IsDestroyedByPlayer()) {
+            pedestal.PedestalDied();
+            AudioSource.PlayClipAtPoint(breakSound, transform.position);
+        }
+        else if (health == PedestalMaxHealth && pedestal.IsDestroyedByPlayer())
+        {
+            PedestalMaxHealth++;
+            health = PedestalMaxHealth;
+            // Let other systems know the enemies repaired a pedestal
+            pedestal.PedestalRepaired();
+            AudioSource.PlayClipAtPoint(restoreSound, transform.position);
+        } else if (healthDelta > 0)
+        {
+            AudioSource.PlayClipAtPoint(healSound, transform.position);
+        } else if (healthDelta < 0)
+        {
+            AudioSource.PlayClipAtPoint(damageSound, transform.position);
         }
 
-        health += healthDelta;
         pedestal.updateVisuals(health, PedestalMaxHealth);
-
-        if (health <= 0) {
-            if (!pedestal.IsDestroyedByPlayer()) {
-                pedestal.PedestalDied();
-            }
-
-            health = 0;
-        }
 
 
         return true;
     }
-
 
 }
