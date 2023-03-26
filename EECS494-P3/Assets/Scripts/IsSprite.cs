6b3aa87ee0b121ecb39c0b17ddb0d74efa54ed9e
jgettig <jgettig@umich.edu>
2023-03-25 23:31:42 -0400

    Did some UI redesign work

--- EECS494-P3/Assets/Scripts/IsSprite.cs
+++ EECS494-P3/Assets/Scripts/IsSprite.cs
@@ -6,8 +6,6 @@ public class IsSprite : MonoBehaviour
 {
     void Start()
     {
-        Vector3 l = transform.localScale;
-        transform.localScale = new Vector3(l.x, l.y / CameraStretch.instance.height, l.z);
-        Debug.Log(transform.localScale);
+        transform.localScale = new Vector3(1, 1 / CameraStretch.instance.height, 1);
     }
 }
