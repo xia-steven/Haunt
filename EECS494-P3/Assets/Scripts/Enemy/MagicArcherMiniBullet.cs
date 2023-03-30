c5560ef9560e447948e0a08fa3846310b1054041
Caleb <calebgif@umich.edu>
2023-03-30 18:07:05 -0400

    Finished the magicArcher enemy

---
+++ EECS494-P3/Assets/Scripts/Enemy/MagicArcherMiniBullet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicArcherMiniBullet : MonoBehaviour
+{
+    MagicArcherProjectile parent;
+
+    private void Awake()
+    {
+        parent = GetComponentInParent<MagicArcherProjectile>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        parent.OnTriggerEnter(other);
+    }
+}
+
