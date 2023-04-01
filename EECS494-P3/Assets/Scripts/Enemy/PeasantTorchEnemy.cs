1c78aa294ed9878238141cf637f10e52a0902ad8
xia-steven <stevenx@umich.edu>
2023-04-01 19:28:59 -0400

    add pesant pitchfork

--- EECS494-P3/Assets/Scripts/Enemy/PeasantTorchEnemy.cs
+++ EECS494-P3/Assets/Scripts/Enemy/PeasantTorchEnemy.cs
@@ -3,21 +3,17 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class PeasantTorchEnemy : EnemyBase {
-
     // Override enemy ID to load from config
-    public override int GetEnemyID()
-    {
+    public override int GetEnemyID() {
         return 0;
     }
 
     // Override attack function
-    public override IEnumerator EnemyAttack()
-    {
+    public override IEnumerator EnemyAttack() {
         Debug.Log("Torch enemy starting attack");
 
         // While attacking
-        while(state == EnemyState.Attacking)
-        {
+        while (state == EnemyState.Attacking) {
             // Perform attack here
 
 
