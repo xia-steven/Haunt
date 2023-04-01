e73e9718a4ad6e6bde94d7746490f7bea9b11db1
Ezra Schoeplein <eschoep@umich.edu>
2023-04-01 00:23:25 -0400

    Explosive dash implementation

---
+++ EECS494-P3/Assets/Scripts/Hub/Upgrades/ExplodeUpgrade.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : Upgrade
+{
+    [SerializeField] float explosiveRadius = 2.0f;
+    [SerializeField] bool oneShotEnemies = false;
+    private GameObject player;
+    private Subscription<PlayerDodgeEvent> dodgeEvent;
+
+    protected override void Awake()
+    {
+        dodgeEvent = EventBus.Subscribe<PlayerDodgeEvent>(_OnDodge);
+        base.Awake();
+    }
+
+    protected override void Start()
+    {
+        thisData = typesData.types[(int)UpgradeType.dashExplode];
+        base.Start();
+    }
+
+    protected override void Apply()
+    {
+        player = GameObject.Find("Player");
+        base.Apply();
+    }
+
+    // Explode on dash finish
+    private void _OnDodge(PlayerDodgeEvent e)
+    {
+        if (!e.start)
+        {
+            // Perform hit
+            Collider[] hitColliders = Physics.OverlapSphere(player.transform.position, explosiveRadius);
+
+            foreach (Collider hit in hitColliders)
+            {
+                HasEnemyHealth enemyHit;
+
+                if (hit.TryGetComponent<HasEnemyHealth>(out enemyHit))
+                {
+                    // Kill the enemy
+                    int damageAmount = oneShotEnemies ? -1000 : -1;
+                    enemyHit.AlterHealth(damageAmount);
+                }
+            }
+
+            // Show visual
+            GameObject visual = Resources.Load<GameObject>("Prefabs/EnemyWeapons/ExplosionRadius");
+            GameObject spawnedVisual = Instantiate(visual, player.transform.position, Quaternion.identity);
+
+            IsSprite spriteScaler = spawnedVisual.GetComponentInChildren<IsSprite>();
+            spriteScaler.scale = explosiveRadius * 2;
+
+            Destroy(spawnedVisual, 0.15f);
+        }
+    }
+
+    protected override void OnDestroy()
+    {
+        EventBus.Unsubscribe(dodgeEvent);
+        base.OnDestroy();
+    }
+}
