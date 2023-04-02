1975b891110f0c759e6374554551679dc9234054
Gregory Cowles <cowlesg@umich.edu>
2023-04-02 11:34:06 -0400

    Thorns snake to player when pedestal is repaired

--- EECS494-P3/Assets/Scripts/Pedestal/EnsnarePlayer.cs
+++ EECS494-P3/Assets/Scripts/Pedestal/EnsnarePlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,73 @@ using UnityEngine;
 public class EnsnarePlayer : MonoBehaviour
 {
     private LineRenderer rend;
+    Transform playerShadow;
+    bool flameLit = false;
+
+    private Subscription<PedestalDestroyedEvent> ped_dest_event;
+    private Subscription<PedestalRepairedEvent> ped_rep_event;
+
     // Start is called before the first frame update
     void Start()
     {
-        rend = GetComponent<LineRenderer>();
+        EventBus.Subscribe<PedestalDestroyedEvent>(_OnPedestalDestroyed);
+        EventBus.Subscribe<PedestalRepairedEvent>(_OnPedestalRepaired);
+            
         
+        rend = GetComponent<LineRenderer>();
+        for (int i = 0; i < IsPlayer.instance.transform.childCount; i++)
+        {
+            Transform child = IsPlayer.instance.transform.GetChild(i);
+            if (child.name == "Shadow")
+            {
+                playerShadow = child;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        rend.SetPositions(new Vector3[] { transform.position, IsPlayer.instance.transform.position });
+        if (flameLit)
+        {
+            rend.SetPositions(new Vector3[] { transform.position, playerShadow.position });
+        }
     }
 
+    void _OnPedestalDestroyed(PedestalDestroyedEvent e)
+    {
+        if (e.pedestalUUID == GetComponent<IsPedestal>().getUUID())
+        {
+            flameLit = false;
+            rend.enabled = false;
+        }
+    }
+    
+    void _OnPedestalRepaired(PedestalRepairedEvent e)
+    {
+        Debug.Log(e.pedestalUUID + " == " + GetComponent<IsPedestal>().getUUID());
+        if (e.pedestalUUID == GetComponent<IsPedestal>().getUUID())
+        {
+            rend.enabled = true;
+            StartCoroutine(GrowTowardPlayer());
+        }
+    }
+
+    IEnumerator GrowTowardPlayer()
+    {
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime;
+            rend.SetPositions(new Vector3[] {transform.position, Vector3.Lerp(transform.position, playerShadow.position, t) });
+            yield return null;
+        }
+        flameLit = true;
+    }
+
+    private void OnDestroy()
+    {
+        EventBus.Unsubscribe<PedestalDestroyedEvent>(ped_dest_event);
+        EventBus.Unsubscribe<PedestalRepairedEvent>(ped_rep_event);
+    }
 }
