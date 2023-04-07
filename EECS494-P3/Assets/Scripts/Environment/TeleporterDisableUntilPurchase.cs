ba4d57f4a5220543bf1492e14d385cd20db8e728
Caleb <calebgif@umich.edu>
2023-04-07 11:59:18 -0400

    Fixed hub world floor and started on disabling teleporter until gun purchased

---
+++ EECS494-P3/Assets/Scripts/Environment/TeleporterDisableUntilPurchase.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(IsTeleporter))]
+public class TeleporterDisableUntilPurchase : MonoBehaviour {
+    IsTeleporter tp;
+    bool activated = false;
+
+    private void Start() {
+        tp = GetComponent<IsTeleporter>();
+
+        tp.Active = false;
+        activated = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!activated && GameControl.NightEnding) {
+            activated = true;
+            tp.Active = true;
+        }
+        else if (!GameControl.NightEnding) {
+            tp.Active = false;
+            activated = false;
+        }
+    }
+}
