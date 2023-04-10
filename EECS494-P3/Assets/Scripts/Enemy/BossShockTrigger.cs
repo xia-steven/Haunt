a40b89fd86677decffa562821f0e69aa1ab52e59
Caleb <calebgif@umich.edu>
2023-04-09 21:06:52 -0400

    Finished the boss attacks

---
+++ EECS494-P3/Assets/Scripts/Enemy/BossShockTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShockTrigger : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // On trigger enter for laser and shockwave
+        PlayerHasHealth playerHealth = other.GetComponent<PlayerHasHealth>();
+        if (playerHealth != null)
+        {
+            Debug.Log("Player damaged from boss");
+            playerHealth.AlterHealth(-1, DeathCauses.Enemy);
+        }
+
+    }
+}
