73b80455e2ffe7947acbe9349be850cc7c3d7488
Ezra Schoeplein <eschoep@umich.edu>
2023-04-15 19:26:34 -0400

    Sniper trail addition

---
+++ EECS494-P3/Assets/Scripts/Weapons/SniperTrail.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperTrail : MonoBehaviour
+{
+    private LineRenderer lr;
+    private float animationDelay = 0.2f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    public void SetTrail(Vector3 target, Vector3 weapon)
+    {
+        lr.SetPositions(new Vector3[] { weapon, target });
+
+        StartCoroutine(TrailAnimation());
+    }
+
+    private IEnumerator TrailAnimation()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            lr.material = Resources.Load<Material>("SniperTrail/SniperTrail" + i);
+            yield return new WaitForSeconds(animationDelay);
+        }
+        Destroy(gameObject);
+    }
+}
