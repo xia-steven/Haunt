2a8fe48b4b5ec6cc32d7d223ba4b0be579aba4ea
xia-steven <stevenx@umich.edu>
2023-04-15 23:00:54 -0400

    added bloom

--- EECS494-P3/Assets/Scripts/CameraStretch.cs
+++ EECS494-P3/Assets/Scripts/CameraStretch.cs
@@ -3,32 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class CameraStretch : MonoBehaviour
-{
+public class CameraStretch : MonoBehaviour {
     public static CameraStretch instance;
 
-    Camera cam;
+    private Camera cam;
     public float height = 1f;
     public float width = 1f;
 
     // Use this for initialization
-    void Awake () 
-    {
+    private void Awake() {
         if (instance == null) instance = this;
-        else Destroy(this);
+        else DestroyImmediate(this);
 
         cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
-    void Update () {
-
+    private void Update() {
         //stretch view//
         cam.ResetProjectionMatrix();
         var m = cam.projectionMatrix;
 
-        m.m11*=height;
-        m.m00*=width;
+        m.m11 *= height;
+        m.m00 *= width;
         cam.projectionMatrix = m;
     }
 }
